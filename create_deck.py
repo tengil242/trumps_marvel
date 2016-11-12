6b133aeebdf35c0ca4a859388a0bed76a540d23e
Ola TeNGiL Tenglin <ola@swdevsupport.se>
2016-11-12 16:33:59 +0100

    Updated. Issue #1 + Issue #5

--- create_deck.py
+++ create_deck.py
@@ -6,15 +6,21 @@
 ##
 ##  Author: Ola TeNGiL Tenglin
 ##
+##  Projects homepage : https://github.com/tengil242/trumps_marvel
+##
 ## 0.0.1 2016-11-11 : First version seems to do the basics for generation
+## 0.0.2 2016-11-12 : Creation of YAML files. Issue #1 - Github
+## 0.0.3 2016-11-12 : Padding of filenumbering. Issue #5 - Github
 ##
-##
+
 import yaml
 import random
 import string
 import pprint
 import os
 import re
+import sys
+import time
 
 from bs4 import BeautifulSoup
 import urllib
@@ -22,6 +28,7 @@ import urllib
 base_url = 'http://marvel.com'
 toc_url = base_url + '/universe3zx/index.php?title=Category:People&from='
 card_root = "./cards/generated"
+VERBOSE_MODE = False
 
 
 def calc_power_attrib(style):    # Raw CSS style value
@@ -121,33 +128,77 @@ for i in range(len(alpha_string)):
 		card_data_eyecolor = data1_entries[10].get_text().replace("Eyes","").strip()
 		card_data_haircolor = data1_entries[11].get_text().replace("Hair","").strip()
 
-		print "  Real name : %s" % card_data_real_name
-		print "  Identity : %s" % card_data_identity
-		print "  Citizenship : %s" % card_data_citizenship
-		print "  Place of Birth : %s" % card_data_placeofbirth
-		print "  First Appearance : %s" % card_data_firstapparance
-		print "  Year of first appearance : %s" % card_data_firstapparance_year
-		print "  Origin : %s" % card_data_origin
-		print "  Height : %s" % card_data_height
-		print "  Weight : %s" % card_data_weight
-		print "  Eye color : %s" % card_data_eyecolor
-		print "  Hair : %s" % card_data_haircolor
-		print "   Bio : %s" % div2_card.get_text()
-		print "  Durability : %s" % card_data_durability
-		print "  Energy Projection : %s" % card_data_energy
+		if VERBOSE_MODE:
+			print "  Real name : %s" % card_data_real_name
+			print "  Identity : %s" % card_data_identity
+			print "  Citizenship : %s" % card_data_citizenship
+			print "  Place of Birth : %s" % card_data_placeofbirth
+			print "  First Appearance : %s" % card_data_firstapparance
+			print "  Year of first appearance : %s" % card_data_firstapparance_year
+			print "  Origin : %s" % card_data_origin
+			print "  Height : %s" % card_data_height
+			print "  Weight : %s" % card_data_weight
+			print "  Eye color : %s" % card_data_eyecolor
+			print "  Hair : %s" % card_data_haircolor
+			print "   Bio : %s" % div2_card.get_text()
+			print "  Durability : %s" % card_data_durability
+			print "  Energy Projection : %s" % card_data_energy
+			print "  Fighting skills : %s" % card_data_fighting
+			print "  Intelligence : %s" % card_data_intelligence
+			print "  Speed : %s" % card_data_speed
+			print "  Strength : %s" % card_data_strength
 
-		print "  Fighting skills : %s" % card_data_fighting
-		print "  Intelligence : %s" % card_data_intelligence
-		print "  Speed : %s" % card_data_speed
-		print "  Strength : %s" % card_data_strength
+		current_time = time.strftime("%Y-%m-%d %H:%M")
 
-		local_fn = card_root + "/" + "card" + str(entry_cntr) + ".jpg"
-		print " (Downloading image file to %s )" % local_fn
-		urllib.urlretrieve (card_data_image_url, local_fn)
+		card = dict(
+    		deck = 'Marvel Universe',
+    		modified = current_time,
+    		character = dict(
+        		name = entry.a['title'],
+				real_name = card_data_real_name,
+				identity = card_data_identity,
+				citizenship = card_data_citizenship,
+				place_of_birth = card_data_placeofbirth,
+				first_apperance = card_data_firstapparance,
+				first_apperance_year = card_data_firstapparance_year,
+				origin = card_data_origin,
+				height = card_data_height,
+				weight = card_data_weight,
+				eye_color = card_data_eyecolor,
+				hair_color = card_data_haircolor,
+				weapons = "0",
+        		bio = div2_card.get_text(),
+        		character_type = 'zum',
+    			),
+			attributes = dict(
+				movies = "0",
+				series = "0",
+				issues = "0",
+				arch_enemies = "0",
+			),
+			powers = dict(
+  				durability = card_data_durability,
+  				energy_projection = card_data_energy,
+  				fighting_skills = card_data_fighting,
+  				intelligence = card_data_intelligence,
+  				speed = card_data_speed,
+  				strength = card_data_strength,
+			),
+			main_url = "http://marvel.com/universe/Spider-Man_(Peter_Parker)",
+			additional_url = "http://comicvine.gamespot.com/spider-man/4005-1443/",
+			image_url = "https://i.annihil.us/u/prod/marvel//universe3zx/images/f/f8/SpiderMan0001.jpg"
+			)
+		cntr='{0:06d}'.format(entry_cntr)
+		fn_image = card_root + "/" + "card" + str(cntr) + ".jpg"
+		print " Downloading image file of %s to %s" % (entry.a['title'],fn_image)
+		urllib.urlretrieve (card_data_image_url, fn_image)
+		fn_card = card_root + "/" + "card" + str(cntr) + ".yaml"
+		print " Generating card of %s to %s" % (entry.a['title'],fn_card)
+
+		with open(fn_card, 'w') as outfile:
+			yaml.safe_dump(card, outfile, allow_unicode=True, default_flow_style=False)
 
 		os._exit(1)
 
 
-
-
-	os._exit(1)
+os._exit(1)
